ab4641e178688be6a5a44c5b2778b195a79f6da0
Leandro Martini <leandromartini86@gmail.com>
2024-09-14 12:36:51 -0300

    preparando para deployar

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
     images: {
         domains: ['res.cloudinary.com','master--altapinta1.netlify.app', 'another-domain.com'],
       },
+      output: 'export',
       
     
 };
