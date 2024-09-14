707bc7f90ada0788196c4532fa6e113858447501
Leandro Martini <leandromartini86@gmail.com>
2024-09-14 13:31:42 -0300

    probando deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['res.cloudinary.com','master--altapinta1.netlify.app', 'another-domain.com'],
-      },
-      output: 'export',
-      
-    
+  // Configuración para manejar imágenes
+  images: {
+    // Lista de dominios permitidos para las imágenes
+    domains: [
+      "res.cloudinary.com",
+      "master--altapinta1.netlify.app",
+      "another-domain.com",
+    ],
+    // Desactiva la optimización de imágenes para exportaciones estáticas
+    unoptimized: true,
+  },
+  // Configuración para exportar una aplicación estática
+  output: 'export',
 };
-
 
 export default nextConfig;
